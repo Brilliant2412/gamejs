d45a012b7f2014165f34957fad02d844d0fd31fa
Hung <miaooo.sl.ys.lyn.hyeon.lb@gmail.com>
2020-08-20 22:26:22 +0700

    Update game.js

--- game.js
+++ game.js
@@ -40,11 +40,37 @@ const startBtn = {
 }
 // control game
 
-document.addEventListener("keydown",moveUp);
+document.addEventListener("keydown",(evt)=>{
+    switch(state.current){
+        case state.getReady:
+            state.current = state.game;
+            SWOOSHING.play();
+            break;
+        case state.game:
+            if (bird.y - bird.radius <= 0){
+                return;
+            }
+            bird.flap();
+            
+            FLAP.play();
+            break;
+        case state.over:
+            let rect = cvs.getBoundingClientRect();
+            let clickX = evt.clientX - rect.left;
+            let clickY = evt.clientY - rect.top;
+            
+            // CHECK IF WE CLICK ON THE START BUTTON
+            if(clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >= startBtn.y && clickY <= startBtn.y + startBtn.h){
+                pipes.reset();
+                bird.speedReset();
+                score.reset();
+                state.current = state.getReady;
+            }
+            break;
+    }
+});
 
-function moveUp(){
-    
-}
+
 
 cvs.addEventListener("click",function(evt){
     switch(state.current){
@@ -57,6 +83,7 @@ cvs.addEventListener("click",function(evt){
                 return;
             }
             bird.flap();
+            
             FLAP.play();
             break;
         case state.over:
